9ef81b7f0f4335eac66f2d394891532ae644fdb6
enhe <dev@iluckin.cn>
2023-02-12 15:56:44 +0800

    fix

--- app/Models/UserReview.php
+++ app/Models/UserReview.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserReview extends Model
 {
+    protected $casts = ['images' => 'array'];
+
     public function user()
     {
         return $this->hasOne(User::class, 'id', 'user_id');
