8e035a6bc012355a5e8ff819e257c3c8225ce421
enhe <dev@iluckin.cn>
2023-02-20 16:48:03 +0800

    paid

--- database/migrations/2023_02_12_111228_create_users_table.php
+++ database/migrations/2023_02_12_111228_create_users_table.php
@@ -16,12 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->char('name', 60)->nullable();
-            $table->char('mobile', 15)->nullable();
+            $table->char('mobile', 15)->nullable()->index();
             $table->unsignedBigInteger('droplet')->default(0);
             $table->string('password')->nullable();
             $table->string('avatar', 400)->nullable();
-            $table->char('openid', 64)->nullable();
-            $table->char('unionid', 64)->nullable();
+            $table->char('openid', 64)->nullable()->index();
+            $table->char('unionid', 64)->nullable()->index();
             $table->unsignedTinyInteger('status')->default(1);
             $table->unsignedTinyInteger('src')->default(0);
             $table->timestamps();
